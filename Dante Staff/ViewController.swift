9d404522095953af80e6db344a6f3cf22cad674a
HungP <HungP>
2019-07-16 23:08:42 -0700

    added done

--- Dante Staff/ViewController.swift
+++ Dante Staff/ViewController.swift
@@ -9,13 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var myTextField: UITextField!
+
+    @IBOutlet weak var phoneTextField: UITextField!
+    @IBOutlet weak var pinTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    @objc func doneButtonAction(){
+        phoneTextField.resignFirstResponder()
+        pinTextField.resignFirstResponder()
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.myTextField.layer.cornerRadius = 10.0
-//        let paddingView = UIView(frame: CGRectMake(0, 0, 15, self.myTextField.frame.height))
+        self.phoneTextField.layer.cornerRadius = 10.0
+        self.pinTextField.layer.cornerRadius = 10.0
+        self.loginButton.layer.cornerRadius = 10.0
+        
+        self.addDoneButtonOnKeyboard()
+
     }
+    
+    func addDoneButtonOnKeyboard()
+    {
+        let doneToolbar: UIToolbar = UIToolbar(frame: CGRect.init(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 50))
+        doneToolbar.barStyle = .default
+        
+        let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let done: UIBarButtonItem = UIBarButtonItem(title: "Done", style: .done, target: self, action: #selector(self.doneButtonAction))
+        
+        let items = [flexSpace, done]
+        doneToolbar.items = items
+        doneToolbar.sizeToFit()
+        
+        phoneTextField.inputAccessoryView = doneToolbar
+        pinTextField.inputAccessoryView = doneToolbar
+    }
+
 
 
 }
