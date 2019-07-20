8384831848657ccec7b052397a386b48b3950882
HungP <HungP>
2019-07-19 23:52:19 -0700

    mainmenu_vc is done

--- Dante Staff/MainMenu_VC.swift
+++ Dante Staff/MainMenu_VC.swift
@@ -10,8 +10,25 @@ import UIKit
 
 class MainMenu_VC: UIViewController {
 
+
+    @IBOutlet weak var view1x1: UIView!
+    @IBOutlet weak var view1x2: UIView!
+    @IBOutlet weak var view2x1: UIView!
+    @IBOutlet weak var view2x2: UIView!
+    @IBOutlet weak var view3x1: UIView!
+    @IBOutlet weak var view3x2: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view1x1.layer.cornerRadius = 10.0
+        view1x2.layer.cornerRadius = 10.0
+        view2x1.layer.cornerRadius = 10.0
+        view2x2.layer.cornerRadius = 10.0
+        view3x1.layer.cornerRadius = 10.0
+        view3x2.layer.cornerRadius = 10.0
+        
     }
+
 
 }
