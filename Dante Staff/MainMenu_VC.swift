edbfbb3bc02c92d90cac45097c64d008d85b0da9
HungP <HungP>
2019-07-22 17:38:31 -0700

    added button change color when pressed

--- Dante Staff/MainMenu_VC.swift
+++ Dante Staff/MainMenu_VC.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class MainMenu_VC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     @IBOutlet weak var view1x1: UIView!
@@ -15,6 +16,9 @@ class MainMenu_VC: UIViewController, UIImagePickerControllerDelegate, UINavigati
     @IBOutlet weak var view2x2: UIView!
     @IBOutlet weak var view3x1: UIView!
     @IBOutlet weak var view3x2: UIView!
+    
+    var viewColor : UIColor!
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(true)
         // Show the Navigation Bar
@@ -26,15 +30,97 @@ class MainMenu_VC: UIViewController, UIImagePickerControllerDelegate, UINavigati
         // Hide the Navigation Bar
         self.navigationController?.setNavigationBarHidden(false, animated: false)
     }
+    
+    @IBAction func btnPressed1x1(_ sender: Any) {
+        self.viewColor = self.view1x1.backgroundColor
+        self.view1x1.backgroundColor = UIColor(white: 1, alpha: 0.5)
+    }
+    @IBAction func btnReleased1x1(_ sender: Any) {
+        view.alpha = 2
+        self.view1x1.backgroundColor = viewColor
+    }
+    @IBAction func dragExit1x1(_ sender: Any) {
+        self.view1x1.backgroundColor = viewColor
+    }
+    @IBAction func btnPressed1x2(_ sender: Any) {
+        self.viewColor = self.view1x2.backgroundColor
+        self.view1x2.backgroundColor = UIColor(white: 1, alpha: 0.5)
+    }
+    @IBAction func btnReleased1x2(_ sender: Any) {
+        view.alpha = 2
+        self.view1x2.backgroundColor = viewColor
+        self.performSegue(withIdentifier: "lookupAcc", sender: self)
+    }
+    
+    @IBAction func dragExit1x2(_ sender: Any) {
+        self.view1x2.backgroundColor = viewColor
+    }
+    @IBAction func btnPressed2x1(_ sender: Any) {
+        self.viewColor = self.view2x1.backgroundColor
+        self.view2x1.backgroundColor = UIColor(white: 1, alpha: 0.5)
+    }
+    @IBAction func btnReleased2x1(_ sender: Any) {
+        view.alpha = 2
+        self.view2x1.backgroundColor = viewColor
+    }
+    @IBAction func dragExit2x1(_ sender: Any) {
+        self.view2x1.backgroundColor = viewColor
+    }
+    @IBAction func btnPressed2x2(_ sender: Any) {
+        self.viewColor = self.view2x2.backgroundColor
+        self.view2x2.backgroundColor = UIColor(white: 1, alpha: 0.5)
+    }
+    @IBAction func btnReleased2x2(_ sender: Any) {
+        view.alpha = 2
+        self.view2x2.backgroundColor = viewColor
+    }
+    
+    @IBAction func dragExit2x2(_ sender: Any) {
+        self.view2x2.backgroundColor = viewColor
+    }
+    @IBAction func btnPressed3x1(_ sender: Any) {
+        self.viewColor = self.view3x1.backgroundColor
+        self.view3x1.backgroundColor = UIColor(white: 1, alpha: 0.5)
+    }
+    @IBAction func btnReleased3x1(_ sender: Any) {
+        view.alpha = 2
+        self.view3x1.backgroundColor = viewColor
+        self.performSegue(withIdentifier: "createAcc", sender: self)
+    }
+    
+    @IBAction func dragExit3x1(_ sender: Any) {
+        self.view3x1.backgroundColor = viewColor
+    }
+    @IBAction func btnPressed3x2(_ sender: Any) {
+        self.viewColor = self.view3x2.backgroundColor
+        self.view3x2.backgroundColor = UIColor(white: 1, alpha: 0.5)
+    }
+    @IBAction func btnReleased3x2(_ sender: Any) {
+        view.alpha = 2
+        self.view3x2.backgroundColor = viewColor
+        try! Auth.auth().signOut()
+        let user = Auth.auth().currentUser
+        if let user = user {
+            print("User is still signed in ======>", user)
+        } else {
+            print("@@@@@@, user does not exist or user is signed out")
+        }
+        self.performSegue(withIdentifier: "signout", sender: self)
+    }
+    @IBAction func dragExit3x2(_ sender: Any) {
+        self.view3x2.backgroundColor = viewColor
+    }
+    
+    
     @IBAction func openCameraBtn(_ sender: UIButton) {
-        
+
         if UIImagePickerController.isSourceTypeAvailable(.camera) {
             let imagePickerController = UIImagePickerController()
             imagePickerController.delegate = self
             imagePickerController.sourceType = .camera
             self.present(imagePickerController, animated: true, completion: nil)
         }
-        
+
     }
 
     override func viewDidLoad() {
