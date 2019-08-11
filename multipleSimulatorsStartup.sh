9d39723c16244c76103fd04b5a37f37528bc7cd2
Hung Phan <hpphann@gmail.com>
2019-08-10 17:04:26 -0700

    launch app to multiple ios simulators at once (app must be individually installed in each simulatorbefore using this method)

---
+++ multipleSimulatorsStartup.sh
@@ -0,0 +1,12 @@
+xcrun simctl shutdown all
+
+path=$(find ~/Library/Developer/Xcode/DerivedData/Dante\ Staff-*/Build/Products/Debug-iphonesimulator -name "Dante Staff.app" | head -n 1)
+echo "${path}"
+
+filename=SimulatorsList.txt
+grep -v '^#' $filename | while read -r line
+do
+xcrun simctl boot "$line" # Boot the simulator with identifier hold by $line var
+xcrun simctl install "$line" "$path" # Install the .app file located at location hold by $path var at booted simulator with identifier hold by $line var
+xcrun simctl launch "$line" com.dante-staff # Launch .app using its bundle at simulator with identifier hold by $line var
+done
